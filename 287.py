e42c358535e6e0db26d0df808d9f9d817ed6e983
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-10 18:59:58 +0500

    Create 287.py

---
+++ 287.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+             
+        return slow
