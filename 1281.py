da8f7bdba3b1408d2d5fb9bbb5fb3efb57015a4f
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-10 17:05:50 +0500

    Create 1281.py

---
+++ 1281.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        sub = 1
+        add = 0
+        for i in str(n):
+            sub *= int(i)
+            add += int(i)
+        return sub-add
