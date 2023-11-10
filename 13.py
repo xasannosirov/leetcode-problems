4cf3978d2673570ae25b890de44b140edd1d84ba
Xasan Nosirov <148367996+xasannosirov@users.noreply.github.com>
2023-11-10 16:24:22 +0500

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        decimal = 0
+        previous_value = 0
+        for i in range(len(s)-1, -1, -1):
+            current_value = roman_values[s[i]]
+            if current_value < previous_value:
+                decimal -= current_value
+            else:
+                decimal += current_value
+            previous_value = current_value
+        return decimal
